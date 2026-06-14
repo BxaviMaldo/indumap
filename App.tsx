47e6bbac6232ed85f37be45e827829012d09fb74
Damaris Peñafiel <pasante.desarrollo@ec.Andersen.com>
2026-06-13 19:03:55 -0500

    hello

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>te amo</Text>
+      <Text>te amo mucho</Text>
       <StatusBar style="auto" />
     </View>
   );
