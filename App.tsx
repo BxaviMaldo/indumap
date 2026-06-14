62e3d8a137d1053406766b4571c5ce36142621a4
BxaviMaldo <bxaviermaldoparedes@gmail.com>
2026-06-13 19:09:55 -0500

    Hola mundo

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>te amo mucho</Text>
+      <Text>te amo mucho yo tambien </Text>
       <StatusBar style="auto" />
     </View>
   );
